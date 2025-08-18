0d76758748d57bd6a4bc0598dbf80d935e504068
JhonKleb <jhonklebersilvacosta0@gmail.com>
2025-08-17 22:05:31 -0300

    Update api.py

--- api.py
+++ api.py
@@ -3,6 +3,8 @@ from flask_restful import Resource, Api, reqparse
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 from datetime import datetime
+import secrets, string
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -21,7 +23,7 @@ class Patrimonio(Resource):
         cur = mysql.connection.cursor()
         cur.execute("SELECT Tombo, Matricula_Serv, Situacao, Data, Local FROM BD_Servidor")
         rows = cur.fetchall()
-        cur.close()
+        cur.close() 
 
         pat_completo = [{'Tombo' : row[0], 'Nome' : row[1], 'Matricula_Serv' : row[2], 'Situacao' : row[3], 'Data': row[4].strftime('%Y-%m-%d %H:%M:%S'), 'Local': row[5]} for row in rows]
         return jsonify({'Patrimônio completo': pat_completo})
@@ -47,6 +49,10 @@ class InserirObjeto(Resource):
         except ValueError:
             raise ValueError("Data deve estar no formato DD-MM-YYYY.")
     
+    def codigo(tamanho=8):
+        caracters = string.ascii_uppercase + string.digits
+        return ''.join(secrets.choice(caracters) for _ in range(tamanho))
+    
     def post(self):
         objeto = reqparse.RequestParser()
         objeto.add_argument('Tombo', type=int, required=True, help="O campo 'Tombo' é obrigatório.")
@@ -54,6 +60,10 @@ class InserirObjeto(Resource):
         objeto.add_argument('Data', type=InserirObjeto.data_valida, required=True, help="O campo 'Data' é obrigatório e deve estar no formato DD-MM-YYYY.")
         objeto.add_argument('Local', type=str, help="O campo 'Local é obrigatório")
 
+        dados = objeto.parse_args()
+        codigo = self.gerar_codigo_ocorrencia()
+
+        return jsonify({'message': 'Objeto adicionado!', 'dados': dados, 'codigo': codigo})
 
 api.add_resource(Patrimonio, '/patrimonio')
 api.add_resource(FiltrarPatrimonio, '/filtpatrimonio')
